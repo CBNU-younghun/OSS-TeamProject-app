dfc40593308825da6f6e2c7969953ee20f34dee5
5jjjjjjmmmmmmm <tryel3652@naver.com>
2024-11-23 15:44:27 +0900

    루틴 추가 시 생성 날짜 추가

--- lib/routine/routine_page.dart
+++ lib/routine/routine_page.dart
@@ -1,4 +1,4 @@
-//루틴 관리 페이지
+// 루틴 관리 페이지
 import 'dart:convert'; // JSON 인코딩 및 디코딩을 위해 사용
 import 'package:flutter/material.dart'; // Flutter의 위젯들을 사용하기 위해 임포트
 import 'package:shared_preferences/shared_preferences.dart'; // 로컬 저장소에 데이터 저장을 위해 사용
@@ -45,6 +45,10 @@ class _RoutinePageState extends State<RoutinePage> {
   // 새로운 루틴을 추가하는 함수
   void _addNewRoutine(Map<String, dynamic> routine) {
     setState(() {
+      // 루틴에 createdAt 필드가 없으면 현재 시간을 추가
+      if (!routine.containsKey('createdAt')) {
+        routine['createdAt'] = DateTime.now().toIso8601String(); // createdAt 필드 추가
+      }
       routines.add(routine); // 새로운 루틴을 리스트에 추가
       _saveRoutines(); // 업데이트된 리스트를 저장
     });
@@ -157,6 +161,11 @@ class _RoutinePageState extends State<RoutinePage> {
           itemCount: routines.length, // 루틴의 개수를 아이템 수로 설정
           itemBuilder: (context, index) {
             final routine = routines[index]; // 각 루틴을 가져옴
+            // 생성 시간을 DateTime 객체로 파싱
+            DateTime createdAt = DateTime.parse(routine['createdAt'] ?? DateTime.now().toIso8601String());
+            // 생성 시간을 원하는 형식으로 포맷
+            String formattedDate = "${createdAt.year}-${createdAt.month}-${createdAt.day} ${createdAt.hour}:${createdAt.minute}";
+
             return GestureDetector(
               onTap: () {
                 _goToRoutineDetailPage(index); // 루틴을 탭하면 상세 페이지로 이동
@@ -175,6 +184,13 @@ class _RoutinePageState extends State<RoutinePage> {
                       fontWeight: FontWeight.bold, // 폰트 굵기 설정
                       fontSize: 18.0, // 폰트 크기 설정
                       fontFamily: 'Roboto', // 폰트 설정
+                    ),
+                  ),
+                  subtitle: Text(
+                    "생성일: $formattedDate", // 생성된 날짜와 시간을 표시
+                    style: TextStyle(
+                      fontSize: 14.0, // 폰트 크기 설정
+                      color: Colors.black54, // 폰트 색상 설정
                     ),
                   ),
                 ),
