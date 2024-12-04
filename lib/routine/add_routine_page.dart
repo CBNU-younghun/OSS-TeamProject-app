d677951c57517f6d9503d2e149fde8d5ecf848ae
jimyung <sjm77773@naver.com>
2024-12-04 22:13:43 +0900

    디자인 변경: 루틴 추가 페이지에서 운동 시간 선택 드롭다운 버튼 디자인 변경 배경 색상 회색 -> 흰색 테두리 추가 '운동 시간 (초)' 텍스트를 선에 걸치도록 하고 박스 안에는 힌트 메시지 출력

--- lib/routine/add_routine_page.dart
+++ lib/routine/add_routine_page.dart
@@ -218,9 +218,9 @@ class _AddRoutinePageState extends State<AddRoutinePage> {
             Container(
               padding: EdgeInsets.symmetric(horizontal: 12.0),
               decoration: BoxDecoration(
-                color: Colors.grey[100],
-                borderRadius: BorderRadius.circular(16.0),
+                color: Colors.white,
               ),
+              width:double.infinity,
               child: DropdownButtonFormField<int>(
                 dropdownColor: Colors.white, // 운동 시간 선택 드롭다운 폼 색상 흰색으로 설정
                 value: selectedTime,
@@ -236,6 +236,7 @@ class _AddRoutinePageState extends State<AddRoutinePage> {
                     ),
                   ),
                 )).toList(),
+                hint: Text('운동 시간 (초) 선택'),
                 onChanged: (value) {
                   setState(() {
                     selectedTime = value;
@@ -249,7 +250,9 @@ class _AddRoutinePageState extends State<AddRoutinePage> {
                     fontSize: 16.0,
                     color: Colors.black87,
                   ),
-                  border: InputBorder.none,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(16.0),
+                  ),
                 ),
               ),
             ),
