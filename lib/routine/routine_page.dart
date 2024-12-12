5b09157dc85abb3b6caef7e59bf7962bc00d5871
5jjjjjjmmmmmmm <tryel3652@naver.com>
2024-12-13 01:48:14 +0900

    주석 개선

--- lib/routine/routine_page.dart
+++ lib/routine/routine_page.dart
@@ -6,104 +6,96 @@ import 'add_routine_page.dart'; // 루틴 추가 페이지를 위한 임포트
 import 'routine_detail_page.dart'; // 루틴 상세 페이지를 위한 임포트
 import '../user_info_page.dart'; // 사용자 정보 페이지를 위한 임포트
 
-// 루틴 관리를 위한 StatefulWidget을 선언
+// 루틴 관리 화면
 class RoutinePage extends StatefulWidget {
   @override
   _RoutinePageState createState() => _RoutinePageState();
 }
 
-// RoutinePage의 상태를 관리하는 클래스
 class _RoutinePageState extends State<RoutinePage> {
-  // 루틴 정보를 담는 리스트를 선언
+  // 저장된 루틴 정보를 담는 리스트
   List<Map<String, dynamic>> routines = [];
 
   @override
   void initState() {
     super.initState();
-    _loadRoutines(); // 위젯이 초기화될 때 저장된 루틴들을 불러옴
+    _loadRoutines(); // 초기화 시 저장된 루틴 불러오기
   }
 
-  // 저장된 루틴들을 로컬 저장소에서 불러오는 함수
+  // 로컬 저장소에서 루틴 데이터를 불러옴
   void _loadRoutines() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     String? savedRoutines = prefs.getString('routines');
     if (savedRoutines != null) {
       setState(() {
-        // JSON 문자열을 맵(Map)의 리스트로 변환하여 routines에 저장
-        routines = List<Map<String, dynamic>>.from(json.decode(savedRoutines) as List);
+        routines = List<Map<String, dynamic>>.from(json.decode(savedRoutines));
       });
     }
   }
 
-  // 현재의 루틴 리스트를 로컬 저장소에 저장하는 함수
+  // 현재 루틴 리스트를 로컬 저장소에 저장
   void _saveRoutines() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    // routines 리스트를 JSON 문자열로 변환하여 저장
     await prefs.setString('routines', json.encode(routines));
   }
 
-  // 새로운 루틴을 추가하는 함수
+  // 새로운 루틴 추가
   void _addNewRoutine(Map<String, dynamic> routine) {
     setState(() {
-      // 루틴에 createdAt 필드가 없으면 현재 시간을 추가
-      if(!routine.containsKey('createdAt')){
-        routine['createdAt'] = DateTime.now().toIso8601String(); // createdAt 필드 추가
+      if (!routine.containsKey('createdAt')) {
+        routine['createdAt'] = DateTime.now().toIso8601String();
       }
-      routines.add(routine); // 새로운 루틴을 리스트에 추가
-      _saveRoutines(); // 업데이트된 리스트를 저장
+      routines.add(routine);
+      _saveRoutines(); // 업데이트 후 저장
     });
   }
 
-  // 기존 루틴을 업데이트하는 함수
+  // 기존 루틴 업데이트
   void _updateRoutine(int index, Map<String, dynamic> updatedRoutine) {
     setState(() {
-      routines[index] = updatedRoutine; // 해당 인덱스의 루틴을 업데이트
-      _saveRoutines(); // 업데이트된 리스트를 저장
+      routines[index] = updatedRoutine;
+      _saveRoutines(); // 변경사항 저장
     });
   }
 
-  // 루틴을 삭제하는 함수
+  // 루틴 삭제
   void _removeRoutine(int index) {
     setState(() {
-      routines.removeAt(index); // 해당 인덱스의 루틴을 리스트에서 제거
-      _saveRoutines(); // 업데이트된 리스트를 저장
+      routines.removeAt(index);
+      _saveRoutines(); // 삭제 후 저장
     });
   }
 
-  // 루틴 추가 페이지로 이동하는 함수
+  // 루틴 추가 페이지로 이동
   void _goToAddRoutinePage() async {
     final result = await Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (context) => AddRoutinePage(), // AddRoutinePage로 이동
-        fullscreenDialog: true, // 전체 화면 다이얼로그로 표시
+        builder: (context) => AddRoutinePage(),
+        fullscreenDialog: true,
       ),
     );
     if (result != null) {
-      _addNewRoutine(result); // 추가된 루틴을 리스트에 반영
+      _addNewRoutine(result); // 새 루틴 추가
     }
   }
 
-  // 루틴 상세 페이지로 이동하는 함수
+  // 루틴 상세 페이지로 이동
   void _goToRoutineDetailPage(int index) async {
     final result = await Navigator.push(
       context,
       MaterialPageRoute(
         builder: (context) => RoutineDetailPage(
-          routine: routines[index], // 선택된 루틴을 전달
-          onSave: (updatedRoutine) {
-            _updateRoutine(index, updatedRoutine); // 루틴이 업데이트되면 리스트에 반영
-          },
-          onDelete: () {
-            _removeRoutine(index); // 루틴이 삭제되면 리스트에서 제거
-          },
+          routine: routines[index],
+          onSave: (updatedRoutine) => _updateRoutine(index, updatedRoutine),
+          onDelete: () => _removeRoutine(index),
         ),
         fullscreenDialog: true,
       ),
     );
     if (result != null) {
       setState(() {
-        _saveRoutines(); // 변경 사항을 저장
+        _saveRoutines(); // 변경 사항 저장
       });
     }
   }
@@ -111,99 +103,85 @@ class _RoutinePageState extends State<RoutinePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // 상단의 앱바를 정의
       appBar: AppBar(
         title: const Text(
-          '루틴 관리', // 앱의 제목을 설정
+          '루틴 관리',
           style: TextStyle(
-            fontFamily: 'Bebas Neue', // 폰트 설정
-            fontSize: 28.0, // 폰트 크기 설정
-            fontWeight: FontWeight.w900, // 폰트 굵기 설정
-            color: Colors.black, // 폰트 색상 설정
+            fontFamily: 'Bebas Neue',
+            fontSize: 28.0,
+            fontWeight: FontWeight.w900,
+            color: Colors.black,
           ),
         ),
-        backgroundColor: Colors.white, // 앱바 배경색 설정
-        iconTheme: const IconThemeData(color: Colors.black), // 아이콘 색상 설정
+        backgroundColor: Colors.white,
+        iconTheme: const IconThemeData(color: Colors.black),
         actions: [
-          // 사용자 정보 페이지로 이동하는 아이콘 버튼을 추가
+          // 사용자 정보 페이지로 이동
           IconButton(
             onPressed: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                  builder: (context) => UserInfoPage(), // UserInfoPage로 이동
+                  builder: (context) => UserInfoPage(),
                   fullscreenDialog: true,
                 ),
               );
             },
-            icon: const Icon(Icons.person, color: Colors.black), // 아이콘 설정
+            icon: const Icon(Icons.person, color: Colors.black),
           ),
         ],
       ),
-      // 본문 영역을 정의
+      // 루틴 리스트 또는 빈 상태 표시
       body: routines.isEmpty
           ? const Center(
-        // 루틴이 없을 경우 표시할 내용
         child: Text(
           '등록된 루틴이 없습니다.',
           style: TextStyle(
-            fontFamily: 'Roboto', // 폰트 설정
-            fontSize: 18.0, // 폰트 크기 설정
-            fontWeight: FontWeight.w600, // 폰트 굵기 설정
-            color: Colors.black54, // 폰트 색상 설정
+            fontFamily: 'Roboto',
+            fontSize: 18.0,
+            fontWeight: FontWeight.w600,
+            color: Colors.black54,
           ),
         ),
       )
           : Padding(
         padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 12.0),
-        // 루틴 목록을 리스트뷰로 표시
         child: ListView.builder(
-          itemCount: routines.length, // 루틴의 개수를 아이템 수로 설정
+          itemCount: routines.length,
           itemBuilder: (context, index) {
-            final routine = routines[index]; // 각 루틴을 가져옴
-            // 생성 시간을 DateTime 객체로 피싱
-            DateTime createdAt = DateTime.parse(routine['createdAt'] ?? DateTime.now().toIso8601String());
-            // 생성 시간을 원하는 형식으로 포맷
-            String formattedDate = "${createdAt.year}-${createdAt.month}-${createdAt.day}-${createdAt.hour}:${createdAt.minute}";
+            final routine = routines[index];
+            DateTime createdAt = DateTime.parse(
+                routine['createdAt'] ?? DateTime.now().toIso8601String());
+            String formattedDate =
+                "${createdAt.year}-${createdAt.month}-${createdAt.day} ${createdAt.hour}:${createdAt.minute}";
             return GestureDetector(
-              onTap: () {
-                _goToRoutineDetailPage(index); // 루틴을 탭하면 상세 페이지로 이동
-              },
+              onTap: () => _goToRoutineDetailPage(index),
               child: Card(
-                color: Colors.white, // 추가된 루틴 폼 배경 색상 흰색으로 설정
-                elevation: 4.0, // 그림자 효과를 추가
+                color: Colors.white,
+                elevation: 4.0,
                 margin: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(16.0), // 카드의 모서리를 둥글게 설정
-                  side: const BorderSide(
-                    color: Colors.black, // 테두리 색상을 검정색으로 설정
-                    width: 1.0, // 테두리 두께 설정
-                  ),
+                  borderRadius: BorderRadius.circular(16.0),
+                  side: const BorderSide(color: Colors.black, width: 1.0),
                 ),
                 child: ListTile(
-                  contentPadding: const EdgeInsets.all(16.0), // 콘텐츠의 여백을 설정
+                  contentPadding: const EdgeInsets.all(16.0),
                   title: Text(
-                    routine['name'], // 루틴의 이름을 표시
+                    routine['name'],
                     style: const TextStyle(
-                      fontWeight: FontWeight.bold, // 폰트 굵기 설정
-                      fontSize: 18.0, // 폰트 크기 설정
-                      fontFamily: 'Roboto', // 폰트 설정
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                      fontFamily: 'Roboto',
                     ),
                   ),
                   subtitle: Text(
-                    "생성일: $formattedDate", // 생성된 날짜와 시간을 표시
-                    style: const TextStyle(
-                      fontSize: 14.0, // 폰트 크기 설정
-                      color: Colors.black54,//
-                    ),
+                    "생성일: $formattedDate",
+                    style: const TextStyle(fontSize: 14.0, color: Colors.black54),
                   ),
-                  // 오른쪽에 삭제 아이콘 버튼 추가
-                  trailing:  IconButton(
+                  trailing: IconButton(
                     icon: const Icon(Icons.delete),
                     color: Colors.black,
-                    onPressed: () {
-                      _showDeleteRoutineDialog(index); // 삭제 확인 다이얼로그 출력
-                    },
+                    onPressed: () => _showDeleteRoutineDialog(index),
                   ),
                 ),
               ),
@@ -211,64 +189,47 @@ class _RoutinePageState extends State<RoutinePage> {
           },
         ),
       ),
-      // 화면 하단에 플로팅 액션 버튼을 추가
+      // 루틴 추가 버튼
       floatingActionButton: FloatingActionButton(
-        onPressed: _goToAddRoutinePage, // 버튼을 누르면 루틴 추가 페이지로 이동
-        backgroundColor: Colors.blueAccent, // 버튼 배경색 설정
-        child: const Icon(
-          Icons.add, // 플러스 아이콘을 표시
-          color: Colors.white, // 아이콘 색상 설정
-        ),
+        onPressed: _goToAddRoutinePage,
+        backgroundColor: Colors.blueAccent,
+        child: const Icon(Icons.add, color: Colors.white),
       ),
     );
   }
 
-  // 루틴 삭제 확인 다이얼로그를 보여주는 함수
+  // 삭제 확인 다이얼로그
   void _showDeleteRoutineDialog(int index) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          backgroundColor: Colors.white, // 삭제 확인 다이얼로그 배경 색상 흰색으로 설정
-          title: const Text('삭제 확인',
-            style: TextStyle(
-              fontFamily: 'Roboto',
-              fontWeight: FontWeight.w600,
-              fontSize: 20.0,
-            ),
-          ), // 대화 상자 제목
-          content: const Text('정말 삭제하시겠습니까?',
-            style: TextStyle(
-              fontFamily: 'Roboto',
-              fontSize: 16.0,
-            ),
-          ), // 대화 상자 내용
+          backgroundColor: Colors.white,
+          title: const Text(
+            '삭제 확인',
+            style: TextStyle(fontFamily: 'Roboto', fontWeight: FontWeight.w600, fontSize: 20.0),
+          ),
+          content: const Text(
+            '정말 삭제하시겠습니까?',
+            style: TextStyle(fontFamily: 'Roboto', fontSize: 16.0),
+          ),
           actions: [
             TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(); // 대화 상자 닫기
-              },
-              child: const Text('취소',
-                style: TextStyle(
-                    color: Colors.black,
-                    fontFamily: 'Roboto',
-                    fontSize: 14.0
-                ),
-              ), // 취소 버튼
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text(
+                '취소',
+                style: TextStyle(color: Colors.black, fontFamily: 'Roboto', fontSize: 14.0),
+              ),
             ),
             TextButton(
               onPressed: () {
-                setState(() {
-                  _removeRoutine(index); // 삭제 함수 호출
-                });
-                Navigator.of(context).pop(); // 대화 상자 닫기
+                _removeRoutine(index);
+                Navigator.of(context).pop();
               },
-              child: const Text('삭제',
-                style: TextStyle(
-                    color: Colors.red,
-                    fontFamily: 'Roboto',
-                    fontSize: 14.0
-                ),), // 삭제 버튼
+              child: const Text(
+                '삭제',
+                style: TextStyle(color: Colors.red, fontFamily: 'Roboto', fontSize: 14.0),
+              ),
             ),
           ],
         );
